9645a40e1c1a51892e98e273071c7934f71454d6
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-31 21:25:26 +0530

    first

--- src/index.js
+++ src/index.js
@@ -4,24 +4,23 @@ import { printHelp, printSystem } from "./cli/output-formatter.js";
 import { promptUser } from "./cli/inputer-handler.js";
 import { safeExit } from "./utils/helper.js";
 
-
-
+const message = [];
 
 async function main() {
     renderBanner()
-    printSystem("Type '/help' for commands. Type '/exit' to quite. " )
+    printSystem("Type '/help' for commands. Type '/exit' to quite. ")
 
-    while(true){
+    while (true) {
         const userText = await promptUser();
 
-        if(!userText) continue;
+        if (!userText) continue;
 
-        if(userText.trim().toLowerCase() ==="/exit"){
+        if (userText.trim().toLowerCase() === "/exit") {
             await safeExit(0);
             return;
         }
 
-        if(userText.trim().toLowerCase() ==="/help"){
+        if (userText.trim().toLowerCase() === "/help") {
             printHelp();
             return;
         }
