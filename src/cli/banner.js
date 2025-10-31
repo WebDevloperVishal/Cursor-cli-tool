0d4a6e2654ea6d46939f2331c129f84de9f7a379
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-31 20:39:41 +0530

    first

--- src/cli/banner.js
+++ src/cli/banner.js
@@ -1,3 +1,11 @@
 import figlet from "figlet";
 import boxen from "boxen";
 import chalk from "chalk";
+
+export function renderBanner(){
+    const title = figlet.textSync("OrbitAI", {
+        horizontalLayout: "fillted"
+    })
+
+    console.log(title)
+}
