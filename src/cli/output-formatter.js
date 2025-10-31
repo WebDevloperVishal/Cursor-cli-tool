c753d4387906216c5720dd4d4d4404b44a1a1ea1
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-10-31 21:12:29 +0530

    first

--- src/cli/output-formatter.js
+++ src/cli/output-formatter.js
@@ -1,2 +1,12 @@
 import chalk from "chalk";
 import boxen from "boxen";
+
+export function printSystem(text) {
+  const boxed = boxen(chalk.yellow(text), {
+    padding: 1,
+    margin: 1,
+    borderColor: 'yellow',
+    borderStyle: 'round',
+  });
+  console.log(boxed);
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { renderBanner } from "./cli/banner.js";
+import { printSystem } from "./cli/output-formatter.js";
 
 async function main() {
     renderBanner()
+    printSystem("Type '/help' for commands. Type '/exit' to quite. " )
 }
 
 main();
